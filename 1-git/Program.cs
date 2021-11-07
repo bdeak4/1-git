b73ed308f86a3c927551accba42c15d37f5b4111
Bartol Deak <b@bdeak.net>
2021-11-07 09:26:31 -0800

    created csharp project

---
+++ 1-git/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("2")
+        }
+    }
+}
